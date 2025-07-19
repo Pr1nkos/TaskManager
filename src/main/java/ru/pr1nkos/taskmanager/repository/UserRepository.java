c6c10d0f22a0d788126bf775f3bcef7fa3d463db
Pr1nkos <prinkos1@gmail.com>
2025-07-19 10:42:14 +0300

    tech(dao) using optional for easier workaround NPE  problems

--- src/main/java/ru/pr1nkos/taskmanager/repository/UserRepository.java
+++ src/main/java/ru/pr1nkos/taskmanager/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.pr1nkos.taskmanager.entity.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Member,Long> {
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
