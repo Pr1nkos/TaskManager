553929f98e2a667c2848986e395079443f3b15d9
Pr1nkos <prinkos1@gmail.com>
2025-07-19 10:40:23 +0300

    tech(user) added createdAt and updatedAt fields in member entity

--- src/main/java/ru/pr1nkos/taskmanager/entity/Member.java
+++ src/main/java/ru/pr1nkos/taskmanager/entity/Member.java
@@ -1,19 +1,22 @@
 package ru.pr1nkos.taskmanager.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +24,11 @@ public class Member {
 
     private String username;
     private String password;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
