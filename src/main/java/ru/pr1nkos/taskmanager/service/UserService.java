22b51a6f8243b0056bb9da1df24da7f221a9421c
Pr1nkos <prinkos1@gmail.com>
2025-07-19 10:41:47 +0300

    tech(jpa) annotate method working with dao as transactional

--- src/main/java/ru/pr1nkos/taskmanager/service/UserService.java
+++ src/main/java/ru/pr1nkos/taskmanager/service/UserService.java
@@ -1,8 +1,11 @@
 package ru.pr1nkos.taskmanager.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+import ru.pr1nkos.taskmanager.dto.request.LoginRequest;
 import ru.pr1nkos.taskmanager.entity.Member;
 import ru.pr1nkos.taskmanager.repository.UserRepository;
 
@@ -10,13 +13,22 @@ import ru.pr1nkos.taskmanager.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void save(Member member) {
         userRepository.save(member);
     }
 
+    @Transactional
     public Member findByUsername(String username) {
-        return userRepository.findByUsername(username);
+        return userRepository.findByUsername(username).orElse(null);
     }
+
+    @Transactional(readOnly = true)
+    public boolean authenticate(LoginRequest loginRequest) {
+        return userRepository.findByUsername(loginRequest.username())
+                .map(member -> passwordEncoder.matches(loginRequest.password(), member.getPassword()))
+                .orElse(false);
     }
 }
